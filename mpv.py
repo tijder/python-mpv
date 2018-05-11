52fbbe91999897ff11b3eab69a658c46b8913adb
tijder <tijder@users.noreply.github.com>
2018-05-11 20:54:56 +0200

    Update mpv.py

--- mpv.py
+++ mpv.py
@@ -37,15 +37,16 @@ else:
     # still better than segfaulting, we are setting LC_NUMERIC to "C".
     locale.setlocale(locale.LC_NUMERIC, 'C')
     
-    pathToLib = './.var/app/org.gnome.Builder/cache/gnome-builder/projects/Plex/flatpak/staging/x86_64-master/files/lib/libmpv.so'
+#     pathToLib = './.var/app/org.gnome.Builder/cache/gnome-builder/projects/Plex/flatpak/staging/x86_64-master/files/lib/libmpv.so'
     print(pathToLib)
-    sofile = ctypes.util.find_library(pathToLib)
-    if sofile is None:
-        raise OSError("Cannot find libmpv in the usual places. Depending on your distro, you may try installing an "
-                "mpv-devel or mpv-libs package. If you have libmpv around but this script can't find it, maybe consult "
-                "the documentation for ctypes.util.find_library which this script uses to look up the library "
-                "filename.")
-    backend = CDLL(sofile)
+#     sofile = ctypes.util.find_library(pathToLib)
+#     if sofile is None:
+#         raise OSError("Cannot find libmpv in the usual places. Depending on your distro, you may try installing an "
+#                 "mpv-devel or mpv-libs package. If you have libmpv around but this script can't find it, maybe consult "
+#                 "the documentation for ctypes.util.find_library which this script uses to look up the library "
+#                 "filename.")
+#     backend = CDLL(sofile)
+    backend = CDLL(pathToLib)
     fs_enc = sys.getfilesystemencoding()
 
 
